70c3a7bf4c27f61a7e5886dbc53bd77980aa7c81
Lim Jia Wui <jiawui@Jiawuis-Macbook.local>
2026-08-03 20:37:13 +0800

    Replace calendar time slots with assignment duration

---
+++ FieldFlow_Calendar_Duration_Updated/js/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB2BiCD0Va9TxY9KYtCEh8o4oV5PMiayYU",
+  authDomain: "team-planner-3b86d.firebaseapp.com",
+  projectId: "team-planner-3b86d",
+  storageBucket: "team-planner-3b86d.firebasestorage.app",
+  messagingSenderId: "984036405194",
+  appId: "1:984036405194:web:3ce489ab14e808df726933"
+};
+export const FIRESTORE_PATH = ["planner", "pwa-v1"];
